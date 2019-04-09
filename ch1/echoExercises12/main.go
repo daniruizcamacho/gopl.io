3ae24fb7f219b3dc231aaf396576fa67c97d3e37
Daniel Ruiz Camacho <daniel.ruizcamacho@gmail.com>
2019-04-09 21:01:13 +0200

    Added echo exercises

---
+++ ch1/echoExercises12/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	s, sep := "", " "
+	for index, arg := range os.Args[1:] {
+		s += sep + strconv.Itoa(index) + sep + arg
+	}
+	fmt.Println(s)
+}
